5b868ced42ec8c1d51d815be3922b4fd6eab6391
= <sojwalpatil2006@gmail.com>
2026-04-28 00:43:09 +0530

    removed

--- main/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import *
-urlpatterns = [
-    path("",index,name="index"),
-    path("delete/<int:id>",delete,name="delete"),
-    path("edit/<int:id>",edit,name="edit"),
-]
